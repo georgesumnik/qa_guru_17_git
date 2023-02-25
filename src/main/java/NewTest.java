8bd6f948635de50862e95ea2707cef9bd6380487
George Sumnik <georgesumnik@gmail.com>
2023-02-25 06:42:32 -0500

    Update NewTest.java

--- src/main/java/NewTest.java
+++ src/main/java/NewTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class NewTest {
     @Test
     void newTest() {
-        int i = 3;
+        int i = 42;
         System.out.println("This is the best test");
         Assertions.assertTrue(i>2)
     }
